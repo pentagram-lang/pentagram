3bb39acc51c5d8531cc312b036fbf1fab53c2fd2
Dan Cecile <dancecile@gmail.com>
2023-02-06 23:04:30 -0500

    Use method name and annotations for simple call ♻️

--- bootstrap/pentagram/host/convert.py
+++ bootstrap/pentagram/host/convert.py
@@ -7,28 +7,52 @@ from pentagram.machine import MachineNumber
 from pentagram.machine import MachineStream
 from pentagram.machine import MachineValue
 from typing import Any
+from typing import get_origin
 
 
-def from_python(value: Any) -> MachineValue:
-    if isinstance(value, IOBase):
-        return MachineStream(value)
-    elif isinstance(value, MachineValue):
+def from_python(
+    python_type: type, value: Any
+) -> MachineValue:
+    origin_python_type = (
+        get_origin(python_type) or python_type
+    )
+    if issubclass(origin_python_type, MachineValue):
+        assert isinstance(value, MachineValue)
         return value
-    elif isinstance(value, bytearray):
+    elif issubclass(origin_python_type, IOBase):
+        assert isinstance(value, IOBase)
+        return MachineStream(value)
+    elif issubclass(origin_python_type, bytearray):
+        assert isinstance(value, bytearray)
         return MachineBlob(value)
-    elif isinstance(value, integer):
+    elif issubclass(origin_python_type, integer):
+        assert isinstance(value, integer)
         return MachineNumber(value)
     else:
         raise AssertionError(value)
 
 
-def to_python(value: MachineValue) -> Any:
-    simple_values = (
-        MachineBlob,
-        MachineNumber,
-        MachineStream,
+def to_python(
+    python_type: type, value: MachineValue
+) -> Any:
+    origin_python_type = (
+        get_origin(python_type) or python_type
     )
-    if isinstance(value, simple_values):
+    if issubclass(origin_python_type, MachineValue):
+        assert isinstance(value, MachineValue)
+        return value
+    elif issubclass(origin_python_type, IOBase):
+        assert isinstance(value, MachineStream)
+        return value.value
+    elif issubclass(origin_python_type, bytearray):
+        assert isinstance(value, MachineBlob)
+        return value.value
+    elif issubclass(origin_python_type, integer):
+        assert isinstance(value, MachineNumber)
         return value.value
     else:
         raise AssertionError(value)
+
+
+def from_python_name(name: str) -> str:
+    return name.rstrip("_").replace("_", "-")
--- bootstrap/pentagram/host/simple_call.py
+++ bootstrap/pentagram/host/simple_call.py
@@ -4,12 +4,14 @@ import math
 
 from dataclasses import dataclass
 from dataclasses import field
+from inspect import Signature
 from inspect import signature
 from io import IOBase
 from numpy import int32
 from numpy import integer
 from numpy.typing import NBitBase
 from pentagram.host.convert import from_python
+from pentagram.host.convert import from_python_name
 from pentagram.host.convert import to_python
 from pentagram.interpret.term import next_term
 from pentagram.machine import MachineBinding
@@ -17,17 +19,16 @@ from pentagram.machine import MachineCall
 from pentagram.machine import MachineFrameStack
 from typing import Any
 from typing import Callable
+from typing import get_args
 
 
 @dataclass
 class SimpleHostCall(MachineCall):
     func: Callable[..., Any]
-    parameter_count: int = field(init=False)
+    signature: Signature = field(init=False)
 
     def __post_init__(self) -> None:
-        self.parameter_count = len(
-            signature(self.func).parameters
-        )
+        self.signature = signature(self.func, eval_str=True)
 
     def __call__(
         self, frame_stack: MachineFrameStack
@@ -35,36 +36,47 @@ class SimpleHostCall(MachineCall):
         frame = frame_stack.current
         expression_stack = frame.expression_stack
         try:
-            args = expression_stack.pop_many(
-                self.parameter_count
-            )
-            converted_args = [
-                to_python(arg) for arg in reversed(args)
-            ]
-            results = self.func(*converted_args)
-            if results is None:
-                results = ()
-            elif not isinstance(results, tuple):
+            converted_args = []
+            for parameter in reversed(
+                self.signature.parameters.values()
+            ):
+                arg = expression_stack.pop()
+                converted_args.append(
+                    to_python(parameter.annotation, arg)
+                )
+            results = self.func(*reversed(converted_args))
+            if self.signature.return_annotation is None:
+                result_types: tuple[Any, ...] = ()
+            elif issubclass(
+                self.signature.return_annotation, tuple
+            ):
+                result_types = get_args(
+                    self.signature.return_annotation
+                )
+            else:
+                result_types = (
+                    self.signature.return_annotation,
+                )
                 results = (results,)
-            converted_results = [
-                from_python(result) for result in results
-            ]
+            converted_results = []
+            for i, result_type in enumerate(result_types):
+                converted_results.append(
+                    from_python(result_type, results[i])
+                )
         except Exception as e:
             raise Exception(f"Error calling {self}") from e
         expression_stack.push_many(converted_results)
         next_term(frame_stack)
 
 
-def simple_call(
-    name: str,
-) -> Callable[[Callable[..., Any]], MachineBinding]:
-    def inner(func: Callable[..., Any]) -> MachineBinding:
-        return MachineBinding(name, SimpleHostCall(func))
-
-    return inner
+def simple_call(func: Callable[..., Any]) -> MachineBinding:
+    return MachineBinding(
+        from_python_name(func.__name__),
+        SimpleHostCall(func),
+    )
 
 
-@simple_call("add")
+@simple_call
 def add(
     blob: bytearray, number: integer[NBitBase]
 ) -> bytearray:
@@ -73,7 +85,7 @@ def add(
     return blob
 
 
-@simple_call("cat")
+@simple_call
 def cat(
     initial_blob: bytearray, concatenation_blob: bytearray
 ) -> bytearray:
@@ -82,16 +94,16 @@ def cat(
     return initial_blob
 
 
-@simple_call("nil-blob")
+@simple_call
 def nil_blob() -> bytearray:
     return bytearray()
 
 
-@simple_call("sqrt")
+@simple_call
 def sqrt(x: int32) -> int32:
     return int32(math.sqrt(x))
 
 
-@simple_call("write")
+@simple_call
 def write(stream: IOBase, blob: bytearray) -> None:
     stream.write(blob)
