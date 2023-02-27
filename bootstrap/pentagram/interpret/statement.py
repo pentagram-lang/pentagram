653c106ad7f68b04d40f4cc512fab6df042233a8
Dan Cecile <dancecile@gmail.com>
2023-02-27 00:12:05 -0500

    Fix most tests 🏗️

--- bootstrap/pentagram/interpret/statement.py
+++ bootstrap/pentagram/interpret/statement.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
 from pentagram.guest.call import GuestCall
-from pentagram.interpret.term import interpret_term
-from pentagram.machine import MachineExpressionStack
+from pentagram.interpret.term import (
+    interpret_expression_term,
+)
+from pentagram.interpret.term import next_term
 from pentagram.machine import MachineFrame
 from pentagram.machine import MachineFrameStack
+from pentagram.machine import MachineInstructionPointer
 from pentagram.syntax import SyntaxAssignment
 from pentagram.syntax import SyntaxExpression
 from pentagram.syntax import SyntaxMethodDefinition
@@ -15,55 +18,46 @@ def interpret_statement(
 ) -> None:
     statement = frame_stack.current.statement
     term_index = frame_stack.current.term_index
-    if term_index == 0:
-        interpret_statement_enter(frame_stack)
-    if term_index < len(statement.terms):
-        interpret_term(frame_stack)
-    else:
-        interpret_statement_exit(frame_stack)
+    if isinstance(statement, SyntaxExpression):
+        if term_index < len(statement.terms):
+            interpret_expression_term(
+                frame_stack, statement
+            )
+        else:
+            next_statement(frame_stack)
+    elif isinstance(statement, SyntaxAssignment):
+        if term_index == 0:
+            interpret_assignment_enter(
+                frame_stack, statement
+            )
+        else:
+            interpret_assignment_exit(
+                frame_stack, statement
+            )
+            next_statement(frame_stack)
+    elif isinstance(statement, SyntaxMethodDefinition):
+        interpret_method_definition(frame_stack, statement)
         next_statement(frame_stack)
-
-
-def interpret_statement_enter(
-    frame_stack: MachineFrameStack,
-) -> None:
-    statement = frame_stack.current.statement
-    if isinstance(statement, SyntaxExpression):
-        pass
-    elif isinstance(statement, SyntaxAssignment):
-        interpret_assignment_enter(frame_stack)
-    elif isinstance(statement, SyntaxMethodDefinition):
-        pass
-    else:
-        raise AssertionError(statement)
-
-
-def interpret_statement_exit(
-    frame_stack: MachineFrameStack,
-) -> None:
-    statement = frame_stack.current.statement
-    if isinstance(statement, SyntaxExpression):
-        pass
-    elif isinstance(statement, SyntaxAssignment):
-        interpret_assignment_exit(frame_stack, statement)
-    elif isinstance(statement, SyntaxMethodDefinition):
-        interpret_method_definition_exit(
-            frame_stack, statement
-        )
     else:
         raise AssertionError(statement)
 
 
 def interpret_assignment_enter(
     frame_stack: MachineFrameStack,
+    statement: SyntaxAssignment,
 ) -> None:
-    old_frame = frame_stack.current
-    new_frame = MachineFrame(
-        instruction_pointer=old_frame.instruction_pointer,
-        expression_stack=MachineExpressionStack(values=[]),
-        environment=old_frame.environment,
+    next_term(frame_stack)
+    frame_stack.push(
+        MachineFrame(
+            instruction_pointer=MachineInstructionPointer(
+                block=statement.block,
+                statement_index=0,
+                term_index=0,
+            ),
+            expression_stack=frame_stack.current.expression_stack,
+            environment=frame_stack.current.environment.extend(),
+        )
     )
-    frame_stack.push(new_frame)
 
 
 def interpret_assignment_exit(
@@ -73,19 +67,18 @@ def interpret_assignment_exit(
     expression_stack = frame_stack.current.expression_stack
     environment = frame_stack.current.environment
     for binding in reversed(assignment.bindings):
+        assert expression_stack, binding
         environment[binding.name] = expression_stack.pop()
-    assert len(expression_stack) == 0, expression_stack
-    frame_stack.pop()
 
 
-def interpret_method_definition_exit(
+def interpret_method_definition(
     frame_stack: MachineFrameStack,
     method_definition: SyntaxMethodDefinition,
 ) -> None:
     environment = frame_stack.current.environment
     environment[method_definition.binding.name] = GuestCall(
         definition_environment=environment,
-        definition_block=method_definition.definition_block,
+        definition_block=method_definition.block,
     )
 
 
--- bootstrap/pentagram/interpret/term_test.py
+++ bootstrap/pentagram/interpret/term_test.py
@@ -3,12 +3,11 @@ from __future__ import annotations
 from numpy import int32
 from pentagram.host.simple_call import sqrt
 from pentagram.host.value import PI
-from pentagram.interpret.term import interpret_term
+from pentagram.interpret.term import (
+    interpret_expression_term,
+)
 from pentagram.interpret.test import init_test_frame_stack
-from pentagram.interpret.test import make_test_environment
 from pentagram.machine import MachineExpressionStack
-from pentagram.machine import MachineFrame
-from pentagram.machine import MachineInstructionPointer
 from pentagram.machine import MachineNumber
 from pentagram.machine import MachineValue
 from pentagram.syntax import SyntaxBlock
@@ -16,25 +15,24 @@ from pentagram.syntax import SyntaxComment
 from pentagram.syntax import SyntaxExpression
 from pentagram.syntax import SyntaxIdentifier
 from pentagram.syntax import SyntaxNumber
-from pentagram.syntax import SyntaxTerm
 from typing import cast
-
-
-def init_term_block(term: SyntaxTerm) -> SyntaxBlock:
-    return SyntaxBlock(
-        statements=[SyntaxExpression(terms=[term])]
-    )
 
 
 def test_interpret_number() -> None:
     term = SyntaxNumber(value=int32(100))
     frame_stack = init_test_frame_stack(
-        init_term_block(term),
+        SyntaxBlock(
+            statements=[SyntaxExpression(terms=[term])]
+        ),
         MachineExpressionStack(values=[]),
     )
-    interpret_term(frame_stack)
+    interpret_expression_term(
+        frame_stack, frame_stack.current.statement
+    )
     assert frame_stack == init_test_frame_stack(
-        init_term_block(term),
+        SyntaxBlock(
+            statements=[SyntaxExpression(terms=[term])]
+        ),
         MachineExpressionStack(
             values=[MachineNumber(value=int32(100))]
         ),
@@ -46,11 +44,18 @@ def test_interpret_identifier_value() -> None:
     term = SyntaxIdentifier(name=PI.name)
     expression_stack = MachineExpressionStack(values=[])
     frame_stack = init_test_frame_stack(
-        init_term_block(term), expression_stack
+        SyntaxBlock(
+            statements=[SyntaxExpression(terms=[term])]
+        ),
+        expression_stack,
     )
-    interpret_term(frame_stack)
+    interpret_expression_term(
+        frame_stack, frame_stack.current.statement
+    )
     assert frame_stack == init_test_frame_stack(
-        init_term_block(term),
+        SyntaxBlock(
+            statements=[SyntaxExpression(terms=[term])]
+        ),
         MachineExpressionStack(
             values=[cast(MachineValue, PI.value_or_call)]
         ),
@@ -64,11 +69,18 @@ def test_interpret_identifier_call() -> None:
         values=[MachineNumber(value=int32(16))]
     )
     frame_stack = init_test_frame_stack(
-        init_term_block(term), expression_stack
+        SyntaxBlock(
+            statements=[SyntaxExpression(terms=[term])]
+        ),
+        expression_stack,
     )
-    interpret_term(frame_stack)
+    interpret_expression_term(
+        frame_stack, frame_stack.current.statement
+    )
     assert frame_stack == init_test_frame_stack(
-        init_term_block(term),
+        SyntaxBlock(
+            statements=[SyntaxExpression(terms=[term])]
+        ),
         MachineExpressionStack(
             values=[MachineNumber(value=int32(4))]
         ),
@@ -82,44 +94,20 @@ def test_interpret_comment() -> None:
         values=[MachineNumber(value=int32(10))]
     )
     frame_stack = init_test_frame_stack(
-        init_term_block(term), expression_stack
+        SyntaxBlock(
+            statements=[SyntaxExpression(terms=[term])]
+        ),
+        expression_stack,
     )
-    interpret_term(frame_stack)
+    interpret_expression_term(
+        frame_stack, frame_stack.current.statement
+    )
     assert frame_stack == init_test_frame_stack(
-        init_term_block(term),
+        SyntaxBlock(
+            statements=[SyntaxExpression(terms=[term])]
+        ),
         MachineExpressionStack(
             values=[MachineNumber(value=int32(10))]
         ),
         term_index=1,
-    )
-
-
-def test_interpret_block() -> None:
-    term = init_term_block(SyntaxNumber(value=int32(2)))
-    expression_stack = MachineExpressionStack(
-        values=[MachineNumber(value=int32(10))]
-    )
-    frame_stack = init_test_frame_stack(
-        init_term_block(term), expression_stack
-    )
-    interpret_term(frame_stack)
-    assert len(frame_stack) == 2
-    assert (
-        frame_stack.frames[0]
-        == init_test_frame_stack(
-            init_term_block(term),
-            MachineExpressionStack(
-                values=[MachineNumber(value=int32(10))]
-            ),
-            term_index=1,
-        ).frames[0]
-    )
-    assert frame_stack.frames[1] == MachineFrame(
-        instruction_pointer=MachineInstructionPointer(
-            block=term, statement_index=0, term_index=0
-        ),
-        environment=make_test_environment().extend(),
-        expression_stack=MachineExpressionStack(
-            values=[MachineNumber(value=int32(10))]
-        ),
     )
